90e2502c43839e6dfd5ceb679546f23270d9d2c2
k4rt4k4 <sartori.eduardo@escola.pr.gov.br>
2025-09-15 08:03:17 -0300

    Create exemplo2.c

---
+++ Linguagem C/exemplo2.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a=10,b=3;
+   
+   printf("Soma: %d\n", a+b);
+   printf("Subrtação: %d\n", a-b);
+   printf("Multiplicação: %d\n", a*b);
+   printf("Divisão: %d\n", a/b);
+   printf("Módulo(Resto da Divisão) %d\n", a%b);
+   
+   float c=10.5, d=3.0;
+   printf("Divisão: %.2f\n", c/d);
+
+    return 0;
+}
